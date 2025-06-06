d8caa92ddce9cfa807a4b35fbb7fdc3fafc09ff5
xerxescoder <xerxescode@gmail.com>
2025-06-06 18:35:37 +0330

    added offline page

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,42 @@
+const CACHE_NAME = "offline-cache";
+const OFFLINE_URL = "/offline.html";
+
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing.");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_URL))
+  );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activating.");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  /*  const url = new URL(event.request.url);   
-if (
-    url.origin.includes("clerk.accounts.dev") ||
-    url.origin.includes("clerk.com")
-  ) {
-    return;
-  } */
-  event.respondWith(fetch(event.request));
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          return response;
+        })
+        .catch(() => {
+          return caches.match(OFFLINE_URL);
+        })
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+  }
 });
